4119247753fdc6a0d521dac9109aa47281e5318b
likhitha <likitha.happy96@gmail.com>
2019-05-17 17:40:12 +0530

    Get Categories Feature partially implemented

---
+++ BussinessLogic/CategoriesRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLogic
+{
+    public class CategoriesRepository
+    {
+        public List<workoutcategory> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WorkoutApi/Controllers/CategoriesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using BussinessLogic;
+namespace WorkoutApi.Controllers
+{
+    public class CategoriesController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
